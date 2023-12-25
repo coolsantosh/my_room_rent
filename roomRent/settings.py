76b85c18273b3fd9fcca71c8cdd267f28a3d531b
santoshrsharma867 <santosh.rsharma867@gmail.com>
2023-12-25 13:01:53 -0500

    updated

--- roomRent/settings.py
+++ roomRent/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-y8-frvxpjx(l!cn(%1e+wkhxahj5+xfx4qm2nm*&d643yd(4wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
